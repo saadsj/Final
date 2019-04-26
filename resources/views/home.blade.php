f0304ca46b9bd9015f5c97f7f6607bebda4793c0
saadsj <47307940+saadsj@users.noreply.github.com>
2019-04-25 22:06:13 -0400

    Finalized

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
                                         </div>
                                     </div>
                                @empty
-                                        There are no questions to view,you can create a question.
+                                        There are no questions to view. You can create a question.
                                     </a>
                                     @endforelse
 
