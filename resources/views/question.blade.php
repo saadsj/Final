a9fb1c67079de96fcc90ae8cdaa90f0ec5aa66ed
saadsj <47307940+saadsj@users.noreply.github.com>
2019-05-10 05:59:56 -0400

    question blade updated

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -45,15 +45,18 @@
                     <div class="card-body">
                         @forelse($question->answers as $answer)
                             <div class="card">
-                                <div class="card-body">{{$answer->body}}</div>
-                                <div class="card-footer">
+                                <div class="card-body">
 
-                                    <a class="btn btn-primary float-right"
-                                       href="{{ route('answers.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
-                                        View
-                                    </a>
-
-                                </div>
+                                    <div> <p style="float: left;margin-right:15px">
+                                            @if($answer->user->avatar == false)
+                                                <img src="https://www.logolynx.com/images/logolynx/d4/d4a80a1f2a0d79a8783d2910f69680cf.png"  height="40" width="40">
+                                            @endif
+                                            @if($answer->user->avatar == true)
+                                                <img src="{{$answer->user->avatar}}" height="40" width="40">
+                                            @endif
+                                        </p>
+                                        <p>{{$answer->body}}</p>
+                                    </div>
                             </div>
                         @empty
                             <div class="card">
