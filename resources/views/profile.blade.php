88aa5e185ebca870008d78dd2d0750432a63e7a2
saadsj <47307940+saadsj@users.noreply.github.com>
2019-05-10 05:44:38 -0400

    profile blade updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,7 +5,19 @@
         <div class="row ">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">My Profile</div>
+                    <div class="card-header">
+
+                        <div> <p style="float: left;margin-right:15px">
+                                @if($profile->user->avatar == false)
+                                    <img src="https://www.logolynx.com/images/logolynx/d4/d4a80a1f2a0d79a8783d2910f69680cf.png"  height="40" width="40">
+                                @endif
+                                @if($profile->user->avatar == true)
+                                    <img src="{{$profile->user->avatar}}" height="40" width="40">
+                                @endif
+                            </p>
+                            <p>My Profile</p>
+                        </div>
+                    </div>
 
                     <div class="card-body ">
                         <span class="font-weight-bold">First Name:</span> {{$profile->fname}}</br>
