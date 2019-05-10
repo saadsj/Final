e325668a5f97350075828ed3f384c0022328519c
saadsj <47307940+saadsj@users.noreply.github.com>
2019-05-10 06:28:55 -0400

    google verification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1"><meta name="google-site-verification" content="DOVrfkVHGkZXPGNgW3nAUT5VODLWRT6ub2QjhkykVMc" />
 
         <title>Laravel</title>
 
