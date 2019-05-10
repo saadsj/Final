d022028f458d0e3776cdf7d7632cb33b14f5f5d4
saadsj <47307940+saadsj@users.noreply.github.com>
2019-05-10 05:35:41 -0400

    question blade updated

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -8,8 +8,16 @@
                     <div class="card-header">Question</div>
 
                     <div class="card-body">
-
-                        {{$question->body}}
+                        <div> <p style="float: left;margin-right:15px">
+                                @if($question->user->avatar == false)
+                                    <img src="https://www.logolynx.com/images/logolynx/d4/d4a80a1f2a0d79a8783d2910f69680cf.png"  height="40" width="40">
+                                @endif
+                                @if($question->user->avatar == true)
+                                    <img src="{{$question->user->avatar}}" height="40" width="40">
+                                @endif
+                            </p>
+                            <p>{{$question->body}}</p>
+                        </div>
                     </div>
                     <div class="card-footer">
                         <a class="btn btn-primary float-right"
